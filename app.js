54cafe2e50f1c5dc6c19b5194a034694dbba08b0
4tunetandf-create <4tunetandf@gmail.com>
2026-06-15 19:17:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -363,7 +363,7 @@ function renderHome(profile) {
 
   let html = "";
 
-  html += "こんにちは " + profile.displayName + " さma<br><br>";
+  html += "こんにちは " + profile.displayName + " さま<br><br>";
   html += "<b>登録会員</b><br>";
 
   for (const member of CACHE.members) {
